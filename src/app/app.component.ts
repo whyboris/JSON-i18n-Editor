cebb7691658a2d2284954581bb4336e3083ddf05
Boris Yakubchik <yboris@yahoo.com>
2019-11-25 14:57:57 -0500

    WIP first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+const en: any = require('../assets/en.json');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'intl';
+  data = en;
+
+  context: string[];
+
+  constructor() {
+    console.log(en);
+    console.log(en.how);
+  }
+
+  getKeys(anyObj) {
+    return Object.keys(anyObj);
+  }
+
+
 }
